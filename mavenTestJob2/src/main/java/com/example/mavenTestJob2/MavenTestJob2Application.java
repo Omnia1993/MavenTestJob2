912e1445e1159fc5461447a84d6728bc694edaa5
Omnia <omniaeid266@gmail.com>
2023-11-24 09:21:32 -0600

    mavenTestJOB2 is committed

---
+++ mavenTestJob2/src/main/java/com/example/mavenTestJob2/MavenTestJob2Application.java
@@ -0,0 +1,13 @@
+package com.example.mavenTestJob2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTestJob2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenTestJob2Application.class, args);
+	}
+
+}
